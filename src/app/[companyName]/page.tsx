d7aeeba44db04c05fc24197191121cf876b72bca
MahidunNobi <mdmahidunnobi@gmail.com>
2025-07-02 00:53:10 +0600

    Configure /[companyname] redirect Urls

--- src/app/[companyName]/page.tsx
+++ src/app/[companyName]/page.tsx
@@ -9,14 +9,14 @@ export default async function CompanyPage({ params }: { params: { companyName: s
   await dbConnect();
   const session = await getServerSession(authOptions);
   if (!session || !session.user?.email) {
-    redirect("/login");
+    redirect(`/${companyName}/login`);
   }
   const company = await Company.findOne({ companyName:  { $regex: new RegExp(`^${companyName}$`, "i") } });
   if (!company) {
     return <div className="text-center mt-20 text-2xl font-bold text-red-600">Company not found</div>;
   }
   if (company.email !== session.user.email) {
-    redirect("/login");
+    redirect(`/${companyName}/login`);
   }
   return (
     <div className="flex items-center justify-center min-h-screen">
