d400a39e21842b0ceca5fcc04c5486d0a51be8f0
MahidunNobi <mdmahidunnobi@gmail.com>
2025-07-03 21:51:57 +0600

    Company cannot be created on same EMAIL or NAME

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,16 +2,24 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/db';
 import Company from '@/models/Company';
 import bcrypt from 'bcryptjs';
+import CompanyUser from '@/models/CompanyUser';
 
 export async function POST(req: Request) {
   try {
     await dbConnect();
     const { companyName, address, phoneNumber, website, email, password } = await req.json();
 
-    const existingCompany = await Company.findOne({ email });
+    const existingCompany = await Company.findOne({
+      $or: [
+        { email: email },
+        { companyName }
+      ]
+    });
+    
     if (existingCompany) {
       return NextResponse.json({ message: 'Company already exists' }, { status: 409 });
     }
+   
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
