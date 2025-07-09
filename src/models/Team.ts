24377b88aeaa84447de07aa9a5f566bde3fb6be8
MahidunNobi <mdmahidunnobi@gmail.com>
2025-07-09 20:51:33 +0600

    Create Model: Teams

---
+++ src/models/Team.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document, models } from 'mongoose';
+
+export interface ITeam extends Document {
+  name: string;
+  employees: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const TeamSchema = new Schema<ITeam>({
+  name: { type: String, required: true },
+  employees: [{ type: Schema.Types.ObjectId, ref: 'CompanyUser', required: true, unique: true }],
+}, { timestamps: true });
+
+export default models.Team || mongoose.model<ITeam>('Team', TeamSchema); 
